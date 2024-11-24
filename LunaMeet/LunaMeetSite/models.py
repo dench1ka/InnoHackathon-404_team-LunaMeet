67989497c835d249e527da91c668be619bc0ddd2
9im0n <demanekeforov2006@gmail.com>
2024-11-24 19:23:18 +0300

    change models for comments, planed, Organizers and Visited

--- LunaMeet/LunaMeetSite/models.py
+++ LunaMeet/LunaMeetSite/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Comments(models.Model):
+    event = models.ForeignKey('Event', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
+
+
+class Planed(models.Model):
+    event = models.ForeignKey('Event', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+
+class Organizers(models.Model):
+    event = models.ForeignKey('Event', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+
+
+class Visited(models.Model):
+    event = models.ForeignKey('Event', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+
