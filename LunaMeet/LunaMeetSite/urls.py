2a6a56f35d98350ecb778a1c6590466046561aa9
9im0n <demanekeforov2006@gmail.com>
2024-11-25 22:09:27 +0300

    after confirm accaunt redirect to login page

--- LunaMeet/LunaMeetSite/urls.py
+++ LunaMeet/LunaMeetSite/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('sign-up', views.show_sign_up),
-    path('sign-in', views.show_sign_in),
+    path('sign-in', views.show_sign_in, name='login'),
     path('user-sign-up', views.register),
     path('user-sign-in', views.login),
     path('confirm_email/<uidb64>/<token>/', views.confirm_email, name='confirm_email'),
