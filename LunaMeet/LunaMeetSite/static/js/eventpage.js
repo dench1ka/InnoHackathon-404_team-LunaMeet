2d097cf42b1215a3670284bbb3257fa7a092a866
9im0n <demanekeforov2006@gmail.com>
2024-11-27 23:00:49 +0300

    add marks for events and empty comments cant posts

--- LunaMeet/LunaMeetSite/static/js/eventpage.js
+++ LunaMeet/LunaMeetSite/static/js/eventpage.js
@@ -1,7 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
     const form = document.getElementById('form');
+    const commentInput = document.getElementById('commentInput');
     form.addEventListener('submit', async (event) => {
         event.preventDefault();
+
+        if (!commentInput.value.trim()) {
+            showPopup("Комментарий не может быть пустым.", false);
+            return;
+        }
 
         const formData = new FormData(form);
         const parts = window.location.href.split("/");
@@ -28,6 +34,61 @@ document.addEventListener('DOMContentLoaded', () => {
             showPopup("Коментарий не опубликован", false);
         }
     });
+
+
+
+    const select = document.getElementById('select');
+    const response = await fetch("user-details-by-token", {
+        headers: {
+            "Authorization": sessionStorage.getItem("authToken")
+        }
+    })
+
+    const result = await response.json();
+
+    if (response.ok){
+        select.value = result.mark;
+    }
+    else {
+        select.value = "Не был(а)"
+    }
+    select.addEventListener('change', async (event) => {
+        const choice = select.value;
+        const parts = window.location.href.split("/");
+        const event_id = parts[parts.length - 1];
+        console.log(choice)
+        console.log(event_id)
+        console.log(sessionStorage.getItem("authToken"))
+
+        try{
+            const response = await fetch('mark-event', {
+                method: "POST",
+                body: JSON.stringify({
+                    event_id: event_id,
+                    value: choice
+                }),
+                headers: {
+                    "Authorization": sessionStorage.getItem("authToken")
+                }
+            }); 
+
+            const result = await response.json();
+
+            if (response.ok) {
+                showPopup("Значение установленно.", true);
+            }
+            else if (response.status === 403){
+                windows.location.href = "sign-in";
+            }
+            else if (response.status === 404){
+                console.log(result.message)
+            }
+        }
+        catch (error) {
+            showPopup("Ошибка сервера", false);
+            console.log(error.message);
+        }
+    })
 })
 
 function showPopup(message, isSuccess) {
--- LunaMeet/LunaMeetSite/static/js/mainpage.js
+++ LunaMeet/LunaMeetSite/static/js/mainpage.js
@@ -3,17 +3,17 @@ document.querySelector('.create-btn').addEventListener('click', function() {
     // Переход на страницу добавления события
     window.location.href = 'add-event'; // Замените на путь к странице добавления события
 });
-// Находим кнопку по ID
-document.getElementById('login-button').addEventListener('click', function() {
-    // Перенаправляем на страницу логина
-    window.location.href = '/sign-in'; // Замените '/login' на правильный URL для вашей страницы логина
-});
+
 document.addEventListener('DOMContentLoaded', function () {
     const search_btn = document.getElementById('search-btn');
     search_btn.addEventListener('click', () => {
         const search_inp = document.getElementById('search-input');
         window.location.href = 'search?query=' + search_inp.value;
-    })
+    });
+
+    document.getElementById('login-btn').addEventListener('click', function() {
+        window.location.href = '/sign-in';
+    });
 
     const authToken = sessionStorage.getItem('authToken');
     const actionsContainer = document.querySelector('.actions');
@@ -50,6 +50,7 @@ document.addEventListener('DOMContentLoaded', function () {
           window.location.href = 'add-event'; // Укажите путь к добавлению события
         } else {
           alert('Пожалуйста, войдите в систему, чтобы создать объявление.');
+          window.location.href = "sign-in"
         }
       });
     }
