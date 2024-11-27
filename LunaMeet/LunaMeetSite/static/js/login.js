57c6d2479cda449146a5e411e2a938cc54d41569
9im0n <105603078+9im0n@users.noreply.github.com>
2024-11-27 16:17:40 +0300

    Revert "Feature profile"

--- LunaMeet/LunaMeetSite/static/js/login.js
+++ LunaMeet/LunaMeetSite/static/js/login.js
@@ -1,29 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('form');
+const form = document.getElementById('form');
 
-    form.addEventListener('submit', async (event) => {
-        event.preventDefault();
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
 
-        const formData = new FormData(form);
+    const formData = new FormData(form);
 
-        try {
-            const response = await fetch('user-sign-in', {
-                method: 'POST',
-                body: formData
-            });
+    try {
+        const response = await fetch('http://127.0.0.1:8000/user-sign-in', {
+            method: 'POST',
+            body: formData
+        });
 
-            const result = await response.json();
+        const result = await response.json();
 
-            if (response.ok) {
-                window.location.href = 'http://127.0.0.1:8000';
-                sessionStorage.setItem('authToken', result.token);
-            } else {
-                showPopup(result.error, false);
-            }
-        } catch (error) {
-            showPopup('Ошибка сети. Попробуйте снова позже.', false);
+        if (response.ok) {
+            window.location.href = 'http://127.0.0.1:8000';
+            sessionStorage.setItem('authToken', result.token);
+        } else {
+            showPopup(result.error, false);
         }
-    });
+    } catch (error) {
+        console.log(error);
+        // Покажем всплывающее окно при сетевой ошибке
+        showPopup('Ошибка сети. Попробуйте снова позже.', false);
+    }
 });
 
 // Функция для отображения всплывающего окна
